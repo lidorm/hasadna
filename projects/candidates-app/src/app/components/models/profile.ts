51d68529d2d58c4952cd1f7c874aa2c2ac8b79b1
segalzeyalz <segaleseo@gmail.com>
2018-09-05 21:06:54 +0300

    add

--- projects/candidates-app/src/app/components/models/profile.ts
+++
@@ -1,6 +0,0 @@
-export interface Profile{
-        first_name: string,
-        last_name: string,
-        project: string,
-        image_url: string
-}
--- projects/candidates-app/src/app/components/profiles/UserInterface.ts
+++
@@ -1,9 +0,0 @@
-export interface User{
-    first_name: string; 
-    last_name: string;
-    email: string;
-    project: string;
-    image_url?: string
-    registered?: Date;
-    active: boolean;
-}
